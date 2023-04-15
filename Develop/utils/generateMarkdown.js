deb10a257a2012c73fa851637b5f3c88e3efe9d9
Antonio lu <antonioraylu@outlook.com>
2023-04-15 16:01:38 +1000

    genreateMarkDown.js update: Created function that returns a license badge

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const bagdes = {
+    MIT: ' [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)', 
+    Apache_2 : '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)', 
+    Boost_Software_License_1: '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)', 
+    GNU_GPL_v3: ' [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)', 
+  }
+  return bagdes[input]
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
