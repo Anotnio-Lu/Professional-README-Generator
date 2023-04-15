488480ddf5540f12a170d2a486c95a02a65bf744
Antonio lu <antonioraylu@outlook.com>
2023-04-15 15:59:57 +1000

    genreateMarkDown.js update: created function to generate markdown for readme

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -11,9 +11,59 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(input) {
+  return `
+# ${input.title}
 
+${licenseBadge(input.license)}
+
+## Table of Content
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Description
+${input.description}
+
+## Installation
+The below are the installation instructions.<br>
+${input.installationInstructions}
+
+### Bash Code
+${codeBlock(input.bashInput)}
+
+
+## Usage
+${input.usageInformation}
+
+## Contributing
+The below is information on how to contribute to the project.<br>
+${input.contributionGuideline}
+
+## Tests
+The below is the instructions to test the project<br>
+${input.testInstructions}
+
+## License
+
+This Project is using the<br>
+${input.license}
+
+For more information, please click on the below link:<br>
+${licenseInfo(input.license)}
+
+## Questions
+
+You can checkout my other project in my Git Hub repository:<br>
+${input.gitHubUsername}
+
+If you would like to get in contact with me or ask me questions, please do not hasitate to email me.<br>
+${input.email}
 `;
 }
 
