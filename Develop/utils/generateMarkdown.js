556f0ffae191cffc14b5dc46df1cd3b754dd27c5
Antonio lu <antonioraylu@outlook.com>
2023-04-15 16:02:21 +1000

    genreateMarkDown.js update: Created function that returns a license link

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,7 +12,15 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const links = {
+    MIT: 'https://choosealicense.com/licenses/mit/', 
+    Apache_2: 'https://choosealicense.com/licenses/apache-2.0/', 
+    Boost_Software_License_1: 'https://choosealicense.com/licenses/bsl-1.0/', 
+    GNU_GPL_v3: 'https://choosealicense.com/licenses/gpl-3.0/', 
+  }
+  return links[input]
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
